946e9320da6b319b73ac077deba3b8c6802306cf
mmtakeuchi <mikemtaki@gmail.com>
2020-10-13 20:23:15 -0700

    get shoes reducer

--- src/reducers/shoesReducer.js
+++ src/reducers/shoesReducer.js
@@ -1,5 +1,10 @@
 const shoesReducer = (state = {shoes: []}, action) => {
     switch(action.type) {
+        case 'GET_SHOES':
+            return {
+                ...state,
+                shoes: action.shoes
+            }
 
         default: 
             return state
