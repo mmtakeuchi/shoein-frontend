75d5ec0e3e503cb44e8bd08cdd8bed36063ba32c
mmtakeuchi <mikemtaki@gmail.com>
2020-10-14 15:48:31 -0700

    added shoes to show up on the associated brand

--- src/components/brands/Brand.js
+++ src/components/brands/Brand.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom"
 // import ShoesContainer from '../../containers/ShoesContainer'
 
 const Brand = (props) => {
   // console.log(props.brands)
-  // console.log(props)
 
   if (props.brands && props.brands.length >= 1) {
     const brand = props.brands.find(
       (brand) => brand.id === parseInt(props.match.params.id, 10)
     );
+
+    const shoes = props.shoes.filter(shoe => shoe.brand_id === brand.id)
+    // console.log(shoes)
     // console.log(brand)
     return (
       <div id={brand.id}>
         
         <h3>{brand.name}</h3>
         <img src={brand.image} alt={brand.name} />
+        {shoes.map(shoe => {
+          return (
+            <div key={shoe.id}>
+              <Link to={"/shoes/" + shoe.id}>{shoe.name}</Link><br/>
+              <img src={shoe.image} alt={shoe.name} height="250" width="300"/>
+            </div>
+          )
+        })}
       </div>
     );
   }
