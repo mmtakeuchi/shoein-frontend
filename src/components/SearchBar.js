69ec443a8997feee86d402feaa00f4d435415b1a
mmtakeuchi <mikemtaki@gmail.com>
2020-10-14 19:39:32 -0700

    added onChange and onSubmit functions to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
+
+    state = {
+        search: ""
+    }
+
+    handleSearch = (event) => {
+        this.setState({
+            search: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        this.setState({
+            search: ""
+        })
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="search">Search</label>
-                    <input type="text" name="search"/>
+                    <input type="text" name="search" onChange={this.handleSearch}/>
                     <input type="submit" value="Search" />
                 </form>
             </div>
