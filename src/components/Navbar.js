9206d86467db6a9e322653ab030aa9ce9d7e1cb8
mmtakeuchi <mikemtaki@gmail.com>
2020-10-15 14:50:37 -0700

    configured navbar to working links and position search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,148 @@
-import React from "react";
+// import React from "react";
+// import AppBar from '@material-ui/core/AppBar';
+// import { NavLink } from "react-router-dom";
+
+// const Navbar = () => (
+//   <AppBar>
+//     <nav>
+//     
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       <li>
+//         <NavLink to="/brands">Brands</NavLink>
+//       </li>
+//       <li>
+//         <NavLink to="/shoes">Shoes</NavLink>
+//       </li>
+//       <li>
+//         <NavLink to="/shoes/new">Create Shoe</NavLink>
+//       </li>
+//     </ul>
+//   </nav>
+//   </AppBar>
+  
+// );
+
+// export default Navbar;
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 import { NavLink } from "react-router-dom";
+import Link from '@material-ui/core/Link';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/brands">Brands</NavLink>
-      </li>
-      <li>
-        <NavLink to="/shoes">Shoes</NavLink>
-      </li>
-      <li>
-        <NavLink to="/shoes/new">Create Shoe</NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
-export default Navbar;
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton> */}
+
+        <Typography className={classes.menuButton} variant="h6" noWrap>
+          <NavLink to="/" color="inherit" >Home</NavLink>
+        </Typography>
+         
+        <Typography className={classes.menuButton} variant="h6" noWrap> 
+       <NavLink to="/brands" variant="body2" color="inherit">Brands</NavLink>
+       </Typography>
+       
+       <Typography className={classes.menuButton} variant="h6" noWrap>
+         <NavLink to="/shoes" color="inherit" >Shoes</NavLink>
+         </Typography>
+
+         <Typography className={classes.title} variant="h6" noWrap>
+         <NavLink to="/shoes/new" color="inherit" >Create Shoe</NavLink>
+         </Typography>
+         
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBar;
